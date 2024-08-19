d8fd7bd478945600871b515f4cf911a553d12672
alex_solo <alexeysolokhin94@gmail.com>
2024-08-19 19:09:47 +0700

    Добавлена модель для хранения файлов

--- src/file_handler/models.py
+++ src/file_handler/models.py
@@ -1,5 +1,20 @@
+from typing import Annotated
+from uuid import UUID, uuid4
+
+from sqlalchemy.orm import Mapped, mapped_column
+
 from src.config.database import Base
 
 
 class File(Base):
-	__tablename__ = 'files'
+    __tablename__ = 'files'
+
+    uid: Mapped[UUID] = mapped_column(primary_key=True, default=uuid4)
+    file_path: Mapped[str]
+    original_name: Mapped[str]
+    size: Mapped[int]
+    format: Mapped[Annotated[str, 16]]
+    extension: Mapped[Annotated[str, 16]]
+
+    def __repr__(self):
+        return f'{self.uid} - {self.original_name}.{self.extension}'
